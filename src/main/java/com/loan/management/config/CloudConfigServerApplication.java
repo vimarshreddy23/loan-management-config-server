0731e4f3ffec546d8b4bd0de7ee0cb87cc24aec0
unknown <ctsjavauser283@iiht.tech>
2020-09-14 14:35:05 +0530

    package change

--- src/main/java/com/loan/management/config/CloudConfigServerApplication.java
+++ src/main/java/com/loan/management/config/CloudConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.scb.cts.academy.config;
+package com.loan.management.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
