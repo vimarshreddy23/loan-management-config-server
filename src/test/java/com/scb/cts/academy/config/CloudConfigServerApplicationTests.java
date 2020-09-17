1d977bf76268b2289b0f87ddb2ec7adf8a8f4797
ctsjavauser287 <ctsjavauser287@ctsjavafs135.iiht.tech>
2020-09-17 18:02:20 +0530

    test case updated

--- src/test/java/com/scb/cts/academy/config/CloudConfigServerApplicationTests.java
+++ src/test/java/com/scb/cts/academy/config/CloudConfigServerApplicationTests.java
@@ -1,13 +1,15 @@
 package com.scb.cts.academy.config;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.loan.management.config.CloudConfigServerApplication;
+
+
 class CloudConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+		CloudConfigServerApplication.main(new String[] {});
 	}
 
 }
